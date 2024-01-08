e23709c3375e92058d27beb09a54ba2d2ba2d418
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-01-08 14:22:44 +0100

    struct

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
+type PBM struct {
+	data          [][]bool
+	width, height int
+	magicNumber   string
+}
+
 func main() {
 
+}
+
+func ReadPBM(filename string) (*PBM, error) {
+	// ...
+}
+
+func (pbm *PBM) Size() (int, int) {
+	// ...
+}
+
+func (pbm *PBM) At(x, y int) bool {
+	// ...
+}
+
+func (pbm *PBM) Set(x, y int, value bool) {
+	// ...
+}
+
+func (pbm *PBM) Save(filename string) error {
+	// ...
+}
+
+func (pbm *PBM) Invert() {
+	// ...
+}
+
+func (pbm *PBM) Flip() {
+	// ...
+}
+
+func (pbm *PBM) Flop() {
+	// ...
+}
+
+func (pbm *PBM) SetMagicNumber(magicNumber string) {
+	// ...
 }
