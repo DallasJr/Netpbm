dbb7478b9854005cd0f7ed965a4b70532abba064
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-01-12 11:39:21 +0100

    finished ppm P3

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -34,7 +36,20 @@ func main() {
 	image.Rotate90CW();
 	fmt.Println("Image rotated 90° clockwise:")
 
+	image.DrawTriangle(Point{0, 0}, Point{4, 0}, Point{4, 4}, Pixel{200, 200, 200})
+	image.DrawCircle(Point{9, 9}, 4, Pixel{100, 100, 100})
+	image.DrawRectangle(Point{6, 4}, 3, 3, Pixel{120, 120, 0})
+	image.DrawLine(Point{12, 0}, Point{0, 12}, Pixel{0, 200, 0})
+
 	changed := image.ToPBM();
+	changedpgm := image.ToPGM();
+
+	err = changedpgm.Save("output.pgm")
+	if err != nil {
+		fmt.Println("Error saving the image:", err)
+		return
+	}
+	fmt.Println("Image saved successfully.")
 
 	err = changed.Save("output.pbm")
 	if err != nil {
@@ -459,12 +474,9 @@ func (pgm *PGM) ToPBM() *PBM {
 	for y, _ := range pgm.data {
 		pbm.data = append(pbm.data, []bool{})
 		for x, _ := range pgm.data[y] {
-			val := int(pgm.data[y][x]);
-			if val == 10 {
-				pbm.data[y] = append(pbm.data[y], false);
-			} else {
-				pbm.data[y] = append(pbm.data[y], true);
-			}
+			grayValue := pgm.data[y][x]
+			isBlack := int(grayValue) < pgm.max/2
+			pbm.data[y] = append(pbm.data[y], isBlack)
 		}
 	}
 	return pbm
@@ -652,13 +664,267 @@ func (ppm *PPM) ToPBM() *PBM{
 	for y, _ := range ppm.data {
 		pbm.data = append(pbm.data, []bool{})
 		for x, _ := range ppm.data[y] {
-			val := ppm.data[y][x];
-			if int(val.R) == 0 && int(val.G) == 0 && int(val.B) == 0 {
-				pbm.data[y] = append(pbm.data[y], true);
-			} else {
-				pbm.data[y] = append(pbm.data[y], false);
-			}
+			r, g, b := ppm.data[y][x].R, ppm.data[y][x].G, ppm.data[y][x].B
+			grayValue := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
+			isBlack := grayValue < 128
+			pbm.data[y] = append(pbm.data[y], isBlack)
 		}
 	}
 	return pbm
+}
+
+func (ppm *PPM) ToPGM() *PGM{
+	pgm := &PGM{}
+	pgm.magicNumber = "P2";
+	pgm.height = ppm.height;
+	pgm.width = ppm.width;
+	pgm.max = ppm.max;
+	for y, _ := range ppm.data {
+		pgm.data = append(pgm.data, []uint8{})
+		for x, _ := range ppm.data[y] {
+			r, g, b := ppm.data[y][x].R, ppm.data[y][x].G, ppm.data[y][x].B
+			grayValue := int(0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b))
+			pgm.data[y] = append(pgm.data[y], uint8(grayValue))
+		}
+	}
+	return pgm
+}
+
+type Point struct{
+    X, Y int
+}
+
+func (ppm *PPM) DrawLine(p1, p2 Point, color Pixel) {
+    dx := int(math.Abs(float64(p2.X - p1.X)))
+    dy := int(math.Abs(float64(p2.Y - p1.Y)))
+    var sx, sy int
+    if p1.X < p2.X {
+        sx = 1
+    } else {
+        sx = -1
+    }
+    if p1.Y < p2.Y {
+        sy = 1
+    } else {
+        sy = -1
+    }
+    err := dx - dy
+    for {
+        ppm.data[p1.Y][p1.X] = color
+        if p1.X == p2.X && p1.Y == p2.Y {
+            break
+        }
+        e2 := 2 * err
+        if e2 > -dy {
+            err -= dy
+            p1.X += sx
+        }
+        if e2 < dx {
+            err += dx
+            p1.Y += sy
+        }
+    }
+}
+
+func (ppm *PPM) DrawRectangle(p1 Point, width, height int, color Pixel) {
+    p2 := Point{p1.X + width - 1, p1.Y}
+    p3 := Point{p1.X, p1.Y + height - 1}
+    p4 := Point{p1.X + width - 1, p1.Y + height - 1}
+    ppm.DrawLine(p1, p2, color)
+    ppm.DrawLine(p2, p4, color)
+    ppm.DrawLine(p4, p3, color)
+    ppm.DrawLine(p3, p1, color)
+}
+
+func (ppm *PPM) DrawFilledRectangle(p1 Point, width, height int, color Pixel) {
+    for y := p1.Y; y < p1.Y+height; y++ {
+        for x := p1.X; x < p1.X+width; x++ {
+            ppm.data[y][x] = color
+        }
+    }
+}
+
+func (ppm *PPM) DrawCircle(center Point, radius int, color Pixel) {
+    x := radius
+    y := 0
+    err := 0
+    for x >= y {
+        ppm.data[center.Y+y][center.X+x] = color
+        ppm.data[center.Y+x][center.X+y] = color
+        ppm.data[center.Y+x][center.X-y] = color
+        ppm.data[center.Y+y][center.X-x] = color
+        ppm.data[center.Y-y][center.X-x] = color
+        ppm.data[center.Y-x][center.X-y] = color
+        ppm.data[center.Y-x][center.X+y] = color
+        ppm.data[center.Y-y][center.X+x] = color
+        y++
+        if err <= 0 {
+            err += 2*y + 1
+        }
+        if err > 0 {
+            x--
+            err -= 2*x + 1
+        }
+    }
+}
+
+func (ppm *PPM) DrawFilledCircle(center Point, radius int, color Pixel) {
+    for y := -radius; y <= radius; y++ {
+        for x := -radius; x <= radius; x++ {
+            if x*x+y*y <= radius*radius {
+                px := center.X + x
+                py := center.Y + y
+                if px >= 0 && px < ppm.width && py >= 0 && py < ppm.height {
+                    ppm.data[py][px] = color
+                }
+            }
+        }
+    }
+}
+
+func (ppm *PPM) DrawTriangle(p1, p2, p3 Point, color Pixel) {
+    ppm.DrawLine(p1, p2, color)
+    ppm.DrawLine(p2, p3, color)
+    ppm.DrawLine(p3, p1, color)
+}
+
+func (ppm *PPM) DrawFilledTriangle(p1, p2, p3 Point, color Pixel) {
+    minY := min(p1.Y, min(p2.Y, p3.Y))
+    maxY := max(p1.Y, max(p2.Y, p3.Y))
+    for y := minY; y <= maxY; y++ {
+        x1, x2 := interpolate(p1, p2, p3, y)
+        for x := x1; x <= x2; x++ {
+            if x >= 0 && x < ppm.width && y >= 0 && y < ppm.height {
+                ppm.data[y][x] = color
+            }
+        }
+    }
+}
+
+func (ppm *PPM) DrawPolygon(points []Point, color Pixel) {
+    for i := 0; i < len(points)-1; i++ {
+        ppm.DrawLine(points[i], points[i+1], color)
+    }
+    ppm.DrawLine(points[len(points)-1], points[0], color)
+}
+
+func (ppm *PPM) DrawFilledPolygon(points []Point, color Pixel) {
+    n := len(points)
+    minY := points[0].Y
+    maxY := points[0].Y
+    for i := 1; i < n; i++ {
+        if points[i].Y < minY {
+            minY = points[i].Y
+        }
+        if points[i].Y > maxY {
+            maxY = points[i].Y
+        }
+    }
+    for y := minY; y <= maxY; y++ {
+        intersections := []int{}
+        for i := 0; i < n; i++ {
+            p1 := points[i]
+            p2 := points[(i+1)%n]
+            if (p1.Y <= y && p2.Y > y) || (p2.Y <= y && p1.Y > y) {
+                x := int(float64(p1.X) + float64(y-p1.Y)/float64(p2.Y-p1.Y)*float64(p2.X-p1.X))
+                intersections = append(intersections, x)
+            }
+        }
+        sort.Ints(intersections)
+        for i := 0; i < len(intersections); i += 2 {
+            x1 := intersections[i]
+            x2 := intersections[i+1]
+            for x := x1; x <= x2; x++ {
+                if x >= 0 && x < ppm.width && y >= 0 && y < ppm.height {
+                    ppm.data[y][x] = color
+                }
+            }
+        }
+    }
+}
+
+
+func (ppm *PPM) DrawKochSnowflake(n int, start Point, width int, color Pixel) {
+    p1 := start
+    p2 := Point{start.X + width, start.Y}
+    height := int(float64(width) * math.Sqrt(3.0) / 2.0)
+    p3 := Point{start.X + width / 2, start.Y + height}
+    ppm.drawKochCurve(n, p1, p2, color)
+    ppm.drawKochCurve(n, p2, p3, color)
+    ppm.drawKochCurve(n, p3, p1, color)
+}
+
+func (ppm *PPM) drawKochCurve(n int, p1, p2 Point, color Pixel) {
+    if n == 0 {
+        ppm.DrawLine(p1, p2, color)
+    } else {
+        v := Point{(2*p1.X + p2.X) / 3, (2*p1.Y + p2.Y) / 3}
+        t := Point{(p1.X + 2*p2.X) / 3, (p1.Y + 2*p2.Y) / 3}
+        u := calculateKochPoint(v, t)
+        ppm.drawKochCurve(n-1, p1, v, color)
+        ppm.drawKochCurve(n-1, v, u, color)
+        ppm.drawKochCurve(n-1, u, t, color)
+        ppm.drawKochCurve(n-1, t, p2, color)
+    }
+}
+
+func calculateKochPoint(p1, p2 Point) Point {
+    angle := math.Pi / 3.0
+    dx := float64(p2.X - p1.X)
+    dy := float64(p2.Y - p1.Y)
+    newX := p1.X + int((dx*math.Cos(angle) - dy*math.Sin(angle))/3.0)
+    newY := p1.Y + int((dx*math.Sin(angle) + dy*math.Cos(angle))/3.0)
+    return Point{newX, newY}
+}
+
+func interpolate(p1, p2, p3 Point, y int) (int, int) {
+    var x1, x2 int
+    if p1.Y == p2.Y && p2.Y == p3.Y {
+        x1 = min(p1.X, min(p2.X, p3.X))
+        x2 = max(p1.X, max(p2.X, p3.X))
+        return x1, x2
+    }
+    if p1.Y == p2.Y {
+        x1, x2 = interpolateFlatTopTriangle(p1, p2, p3, y)
+    } else if p2.Y == p3.Y {
+        x1, x2 = interpolateFlatBottomTriangle(p1, p2, p3, y)
+    } else {
+        alpha := float64(p2.Y-p3.Y) / float64(p1.Y-p3.Y)
+        beta := 1.0 - alpha
+        x1a, x2a := interpolateFlatTopTriangle(p1, p2, p3, y)
+        x1b, x2b := interpolateFlatBottomTriangle(p1, p2, p3, y)
+        x1 = int(alpha*float64(x1a) + beta*float64(x1b))
+        x2 = int(alpha*float64(x2a) + beta*float64(x2b))
+    }
+    return x1, x2
+}
+
+func interpolateFlatTopTriangle(p1, p2, p3 Point, y int) (int, int) {
+    alpha := float64(y-p1.Y) / float64(p2.Y-p1.Y)
+    beta := float64(y-p1.Y) / float64(p3.Y-p1.Y)
+    x1 := int(float64(p1.X) + alpha*float64(p2.X-p1.X))
+    x2 := int(float64(p1.X) + beta*float64(p3.X-p1.X))
+    return x1, x2
+}
+
+func interpolateFlatBottomTriangle(p1, p2, p3 Point, y int) (int, int) {
+    alpha := float64(y-p1.Y) / float64(p3.Y-p1.Y)
+    beta := float64(y-p2.Y) / float64(p3.Y-p2.Y)
+    x1 := int(float64(p1.X) + alpha*float64(p3.X-p1.X))
+    x2 := int(float64(p2.X) + beta*float64(p3.X-p2.X))
+    return x1, x2
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
